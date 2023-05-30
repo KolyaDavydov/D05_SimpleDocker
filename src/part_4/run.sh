cf9e6693e015c362df49dd48306f3dc269078b55
Kolya Davydov <kolya.davydov@inbox.ru>
2023-05-30 21:43:35 +0300

    part_4 complite

--- src/part_4/run.sh
+++ src/part_4/run.sh
@@ -2,3 +2,4 @@
 gcc mini_server.c -lfcgi -o mini_server
 spawn-fcgi -p 8080 ./mini_server
 service nginx start
+/bin/bash
--- src/part_4/start.sh
+++ src/part_4/start.sh
@@ -10,13 +10,14 @@ echo "Current images:"
 docker images
 echo
 echo "running container..."
-docker run -d  -p 80:81 --name new_con new:1.0
+docker run -it -p 80:81 --name new_con -d new:1.0
 echo
 echo "Curren container:"
 docker ps
 echo
-echo "Checking result: curl localhost:81:"
+echo "Checking result: curl localhost:80:"
+sleep 1
 curl localhost:80
 echo
 echo "Checking serverstatus:"
-curl localhost:81/status
+curl localhost:80/status
